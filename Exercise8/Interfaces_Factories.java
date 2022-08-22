fa7c287e2bbd994975794cb9bebe110aaf817a8e
wangsuchi <wangsuchi28@gmal.com>
2022-08-22 15:25:21 +0900

    interface and factory

---
+++ Exercise8/Interfaces_Factories.java
@@ -0,0 +1,12 @@
+package Exercise8;
+//Many people have fallen to this temptation, creating interfaces and factories wherever it’s
+//possible. The logic seems to be that you might need to use a different implementation, so you
+//should always add that abstraction. It has become a kind of premature design optimization.
+//Any abstraction should be motivated by a real need. Interfaces should be something you
+//refactor to when necessary, rather than installing the extra level of indirection everywhere,
+//along with the extra complexity. That extra complexity is significant, and if you make
+//someone work through that complexity only to realize that you’ve added interfaces "just in
+//case" and for no compelling reason—well, if I see such a thing I begin to question all the
+//designs that this particular person has done.
+public class Interfaces_Factories {
+}
