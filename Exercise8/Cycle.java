8386aedb24e981aeb8d303f3f216a99129665421
wangsuchi <88770851+wangsuchi@users.noreply.github.com>
2022-11-01 19:23:24 +0900

    Delete Exercise8 directory

--- Exercise8/Cycle.java
+++
@@ -1,7 +0,0 @@
-package Exercise8;
-
-// Create a Cycle interface, with implementations Unicycle, Bicycle
-//and Tricycle. Create factories for each type of Cycle, and code that uses these factories.
-//page 239~ read again
-public interface Cycle {
-}
